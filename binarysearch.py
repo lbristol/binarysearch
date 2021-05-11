a396282809190fd9db18306622409f717fd0b1ef
Liam Bristol <lbristol623@gmail.com>
2021-05-11 06:23:26 -0400

    Overhauled a lot of the code to make it more efficient, added a benchmarking tool to see how it stacks up to "in" in Python3.
    Stuck on an annoying error with my while loop but that's temporary anyway, or so I think...

--- binarysearch.py
+++ binarysearch.py
@@ -2,23 +2,41 @@ import random
 import time
 
 class main:
-    def __init__(self,input):
+    def __init__(self, input, rangeinput):
         self.input = input
+        self.rangeinput = rangeinput
         self.list = []
 
     def randomgen(self):
-        i = random.randint(1,9)
-        self.list = range(i, 1000000, 3)
+        i = random.randint(1, self.rangeinput)
+        # print(i)
+        self.list = range(0, 100000000, i)
+        # for j in range(10):
+        #     print(self.list[j])
+
+    def benchmark(self):
+        start = time.time()
+        if self.input in self.list:
+            end = time.time()
+            return (end-start)/1000
+        else:
+            end = time.time()
+            return (end-start)/1000
+
 
     def find(self):
         start = time.time()
         if len(self.list) == 1:
             if self.input == self.list[0]:
                 end = time.time()
-                return print(str(self.input) + " has been found in " + str((end-start)/1000) + " seconds! Good guess.")
+                finaltime = (end-start)/1000
+                return print(str(self.input) + " has been found. The search took " + str(finaltime) + " seconds. (Binary Search Algorithm; " +
+                    str((finaltime/self.benchmark())*100) + "% the speed of Python3 'in' operator (" + str(self.benchmark()) + " seconds))")
             else:
                 end = time.time()
-                return print(str(self.input) + " has not been found. The search took " + str((end-start)/1000) + " seconds.")
+                finaltime = (end-start)/1000
+                return print(str(self.input) + " has not been found. The search took " + str(finaltime) + " seconds. (Binary Search Algorithm; " +
+                    str((finaltime/self.benchmark())*100) + "% the speed of Python3 'in' operator (" + str(self.benchmark()) + " seconds))")
         elif len(self.list) % 2 == 0:
             h = int(len(self.list) / 2)
         else:
@@ -32,20 +50,41 @@ class main:
             self.find()
 
 
+
+
 while True:
-    x = input("Enter another number? y/n ")
-    if x.isalpha():
-        if x.upper() == "Y":
-            y = input("Enter a non-decimal number here: ")
-            if y.isnumeric() and "." not in y:
-                go = main(int(y))
-                go.randomgen()
-                go.find()
+        x = input("Play again? y/n ")
+        try:
+            if x.upper() == "Y":
+                try:
+                    print("We're going to play a guessing game.")
+                    print("You'll help determine some probabilistic parameters,")
+                    print("a random number generator will determine how many items will show up in a list of numbers ranging from 1 to 100,000,000,")
+                    print("and you'll guess a number. If it's on the list, you win!")
+                    print("\n \n")
+                    print("The interval between items in the list, or step, determines how many items are in the list.")
+                    rangeinput = int(input("Enter a maximum value for the step (interval between items in the list) between 1 and 100 (no decimals): "))
+                    if rangeinput < 1:
+                        print("The number you entered is too low.")
+                    if rangeinput > 100:
+                        print("The number you entered is too high.")
+                    # print(rangeinput)
+                    input = int(input("Enter a positive integer here - this is your guess (remember - no decimals!): "))
+                    if input < 1:
+                        print("The number you entered is too low.")
+                    if input > 100000000:
+                        print("The number you entered is too high.")
+                    # print(input)
+                    go = main(input, rangeinput)
+                    go.randomgen()
+                    # print("randomgen done")
+                    go.find()
+                    # print("find done")
+                except (ValueError, TypeError) as e:
+                    print(e)
+            elif x.upper() == "N":
+                break
             else:
-                print("Looks like you made a typo.")
-        elif x.upper() == "N":
-            break
-        else:
-            print("Looks like you made a typo.")
-    else:
-        print("Looks like you made a typo.")
+                print("ELSE Looks like you made a typo.")
+        except (ValueError, TypeError) as e:
+            print(e)
